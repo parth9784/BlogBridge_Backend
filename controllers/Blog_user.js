7162c8aa62106b24d5f53917f7539a74b4d1b8c8
parth9784 <pdsharma458@gmail.com>
2024-08-03 10:40:56 +0530

    changed the blog_user..prev

--- controllers/Blog_user.js
+++ controllers/Blog_user.js
@@ -2,19 +2,19 @@ const Blog = require('../models/Blogs');
 
 async function Blog_user(req, res) {
     try {
+
         if (!req.userid) {
             return res.status(400).json({ msg: 'User ID is required.' });
         }
+        const data = await Blog.find({ author: req.userid })
+            .populate('Comments')
+            .populate('Like');     
 
-        const blogs = await Blog.find({ author: req.userid })
-            .populate('comments') 
-            .populate('likes'); 
-
-        if (blogs.length === 0) {
-            return res.status(200).json({ msg: 'No blogs found for this user.' });
+        if (data.length === 0) {
+            return res.status(400).json({ msg: 'No blogs found for this user.' });
         }
 
-        res.status(200).json(blogs);
+        res.status(200).json(data);
     } catch (error) {
         console.error(error);
         res.status(500).json({ msg: 'Error retrieving blogs.' });
@@ -22,4 +22,3 @@ async function Blog_user(req, res) {
 }
 
 module.exports = { Blog_user };
-
