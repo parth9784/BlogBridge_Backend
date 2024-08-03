ee1f41421f5431c45faa70d589f0d7c8e5e269d6
parth9784 <pdsharma458@gmail.com>
2024-08-03 10:18:55 +0530

    made changes in blog_user file..

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use("",routes)
 dbconnect()
 
 app.get("/",(req,res)=>{
-    res.send("<h1>This is Home Page....</h1>")
+    res.send("<h1>This is BlogBridge API.... written by Parth Dadhich</h1>")
 })
 app.listen(process.env.PORT,(req,res)=>{
     console.log("Server Started...")
